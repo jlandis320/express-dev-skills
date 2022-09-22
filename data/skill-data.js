ffdd98c0ce82e29cfd73604bf6290a04c68a428d
jlandis320 <jreynoldslandis@gmail.com>
2022-09-22 15:02:02 -0400

    adds skills array

---
+++ data/skill-data.js
@@ -0,0 +1,12 @@
+const skills = [
+  {text: 'Javascript', learned: true},
+  {text: 'CSS', learned: true },
+  {text: 'HTML5', learned: true },
+  {text: 'DOM Manipulation', done: true },
+  {text: 'MongoDB', done: false },
+  {text: 'Python', done: false },
+]
+
+export { 
+	skills
+}
